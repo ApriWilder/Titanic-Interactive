6e535fbb9a48cf56c0d81768dfd0187118cb568a
ApriWilder <april.wilder@gmail.com>
2024-10-30 21:27:02 -0700

    10/30/2024

---
+++ story_content/triggers.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6jerAdMTaPA":
+        Script1();
+        break;
+      case "5z9HE6HCb0b":
+        Script2();
+        break;
+      case "6KYXB629LoO":
+        Script3();
+        break;
+      case "5aL6Rlq9iAW":
+        Script4();
+        break;
+      case "6mt3pYriR3O":
+        Script5();
+        break;
+      case "6rSO13c4gyi":
+        Script6();
+        break;
+      case "5c5YHc0n5Y7":
+        Script7();
+        break;
+      case "5blAZlng56M":
+        Script8();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ story_content/user.js
@@ -0,0 +1,112 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var player = GetPlayer(); // Get the Storyline player
+var booty1Value = player.GetVar("Booty1"); // Fetch the Booty1 variable
+
+// Convert the number to a string and add commas using toLocaleString()
+var formattedValue = Number(booty1Value).toLocaleString('en-US'); 
+
+// Store the formatted value in Bootydisplay for display purposes
+player.SetVar("Booty1display", formattedValue);
+
+}
+
+window.Script2 = function()
+{
+  var player = GetPlayer(); // Get the Storyline player
+var bootyValue = player.GetVar("Booty"); // Fetch the Booty variable
+
+// Convert the number to a string and add commas using toLocaleString()
+var formattedValue = Number(bootyValue).toLocaleString('en-US'); 
+
+// Store the formatted value in Bootydisplay for display purposes
+player.SetVar("Bootydisplay", formattedValue);
+
+}
+
+window.Script3 = function()
+{
+  var player = GetPlayer(); // Get the Storyline player
+var bootyValue = player.GetVar("Booty"); // Fetch the Booty variable
+
+// Convert the number to a string and add commas using toLocaleString()
+var formattedValue = Number(bootyValue).toLocaleString('en-US'); 
+
+// Store the formatted value in Bootydisplay for display purposes
+player.SetVar("Bootydisplay", formattedValue);
+
+}
+
+window.Script4 = function()
+{
+  var player = GetPlayer(); // Get the Storyline player
+var bootyValue = player.GetVar("Booty"); // Fetch the Booty variable
+
+// Convert the number to a string and add commas using toLocaleString()
+var formattedValue = Number(bootyValue).toLocaleString('en-US'); 
+
+// Store the formatted value in Bootydisplay for display purposes
+player.SetVar("Bootydisplay", formattedValue);
+
+}
+
+window.Script5 = function()
+{
+  var player = GetPlayer(); // Get the Storyline player
+var bootyValue = player.GetVar("Booty"); // Fetch the Booty variable
+
+// Convert the number to a string and add commas using toLocaleString()
+var formattedValue = Number(bootyValue).toLocaleString('en-US'); 
+
+// Store the formatted value in Bootydisplay for display purposes
+player.SetVar("Bootydisplay", formattedValue);
+
+}
+
+window.Script6 = function()
+{
+  var player = GetPlayer(); // Get the Storyline player
+var bootyValue = player.GetVar("Booty"); // Fetch the Booty variable
+
+// Convert the number to a string and add commas using toLocaleString()
+var formattedValue = Number(bootyValue).toLocaleString('en-US'); 
+
+// Store the formatted value in Bootydisplay for display purposes
+player.SetVar("Bootydisplay", formattedValue);
+
+}
+
+window.Script7 = function()
+{
+  var player = GetPlayer(); // Get the Storyline player
+var bootyValue = player.GetVar("Booty"); // Fetch the Booty variable
+
+// Convert the number to a string and add commas using toLocaleString()
+var formattedValue = Number(bootyValue).toLocaleString('en-US'); 
+
+// Store the formatted value in Bootydisplay for display purposes
+player.SetVar("Bootydisplay", formattedValue);
+
+}
+
+window.Script8 = function()
+{
+  var player = GetPlayer(); // Get the Storyline player
+var bootyValue = player.GetVar("Booty"); // Fetch the Booty variable
+
+// Convert the number to a string and add commas using toLocaleString()
+var formattedValue = Number(bootyValue).toLocaleString('en-US'); 
+
+// Store the formatted value in Bootydisplay for display purposes
+player.SetVar("Bootydisplay", formattedValue);
+
+}
+
+};
