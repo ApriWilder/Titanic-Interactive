c500f7058a80a15f6fb7ef86c83086bc13cdfd4e
ApriWilder <april.wilder@gmail.com>
2024-11-03 09:16:55 -0800

    updates 11/3/2024

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,31 +2,31 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "63giBsOWhUf":
+      case "5qY3Cpv0JEq":
         Script1();
         break;
-      case "5YR69UuSGtQ":
+      case "6dNGFz2PANX":
         Script2();
         break;
-      case "68JMSeRu2Dz":
+      case "6fEw5VH9r2r":
         Script3();
         break;
-      case "6UZbnrT3KFL":
+      case "6f214diSViQ":
         Script4();
         break;
-      case "6XNPiblxDxq":
+      case "6KO44pX9JF7":
         Script5();
         break;
-      case "6iXXcMsh6cy":
+      case "5jeOYBGQQkc":
         Script6();
         break;
-      case "5jPZXrmtrUg":
+      case "6oGpFofZuW5":
         Script7();
         break;
-      case "5qcGJdIQxsb":
+      case "6qF74yULTcZ":
         Script8();
         break;
-      case "6UMvXdOnwp6":
+      case "5jqHZWB83I2":
         Script9();
         break;
   }
